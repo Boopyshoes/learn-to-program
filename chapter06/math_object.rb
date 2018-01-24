2ee654fbc343823ef448efd5c30e62c4039f7ab9
Dan Martz <dmartz@Boopster.attlocal.net>
2018-01-23 18:15:43 -0600

    finish chapter 6

---
+++ chapter06/math_object.rb
@@ -0,0 +1,6 @@
+puts(Math::PI)
+puts(Math::E) #this is always lowercase in math, but I understand capital letters are for constants here in Ruby-land
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1 + Math.sqrt(5))/2)
---
+++ chapter06/random_numbers.rb
@@ -0,0 +1,19 @@
+# I'm starting to feel like the Learn to Program book doesn't adhere to the conventions in the Ruby style guide
+# I should make some Ruby style flash cards and then go back and edit all this early stuff to be in
+# line with the Ruby Style guide for practice
+
+# srand(1977)
+srand
+puts rand
+puts rand
+puts rand
+puts(rand(100))
+puts(rand(100))
+puts(rand(1))
+puts(rand(1))
+puts(rand(1))
+puts(rand(9999999999999999999999999999999999999999999999999))
+puts('The weatherman said there is a')
+puts(rand(101).to_s + '% chance of rain.')
+puts('but you can never trust the weatherman.')
+
