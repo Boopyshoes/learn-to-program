b4c30abaebb80c43f2887d6d4c6e5ad1bd067eb7
Dan Martz <djmartz77@gmail.com>
2018-01-26 10:44:03 -0600

    add chapter07 and branching.rb

---
+++ chapter07/branching.rb
@@ -0,0 +1,23 @@
+=begin
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris'
+  puts 'What a lovely name!'
+end
+=end
+
+puts 'I am a fortune teller. Tell me your name: '
+name = gets.chomp
+
+if name == 'Dan'
+ 
+  puts 'I see great things in your future.'
+
+else
+  
+  puts 'Your future is... oh my! Look at the time!'
+  puts 'I really have to be going, sorry!'
+
+end
